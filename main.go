c04673da6e78f5552928a530b1113a36c96c1248
DmitryStaroverov <rolton_10@mail.ru>
2025-09-26 19:53:31 +0300

    new commit

--- main.go
+++ main.go
@@ -2,34 +2,47 @@ package main
 
 import "fmt"
 
-var a int
-var b int
-
 func main() {
 	var arr [15]int
-	var arr1 [15]int
-	var arr2 [15]int
+	var arrEven [15]int
+	var arrOdd [15]int
 
+	evenCount := 0
+	oddCount := 0
+
+	// Ввод чисел
+	fmt.Println("Введите 15 чисел:")
 	for i := 0; i < len(arr); i++ {
 		fmt.Scan(&arr[i])
-
 		if arr[i]%2 == 0 {
-			arr2[a] = arr[i]
-			a++
-		} else if arr[i]%2 != 0 {
-			arr1[b] = arr[i]
-			b++
+			arrEven[evenCount] = arr[i]
+			evenCount++
+		} else {
+			arrOdd[oddCount] = arr[i]
+			oddCount++
 		}
-
 	}
-	for i := 0; i < len(arr); i++ {
-		if arr2[i] != 0 {
-			fmt.Print(arr2[i], " ")
-		}
-		if arr1[i] != 0 {
-			fmt.Print(arr1[i], " ")
 
-		}
+	// Два счётчика для чётных и нечётных
+	i, j := 0, 0
 
+	// Вывод поочередно: чётный, нечётный
+	for i < evenCount && j < oddCount {
+		fmt.Print(arrEven[i], " ")
+		i++
+		fmt.Print(arrOdd[j], " ")
+		j++
+	}
+
+	// Если остались чётные
+	for i < evenCount {
+		fmt.Print(arrEven[i], " ")
+		i++
+	}
+
+	// Если остались нечётные
+	for j < oddCount {
+		fmt.Print(arrOdd[j], " ")
+		j++
 	}
 }
